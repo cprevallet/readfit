ce423a687a9bf1620b433c1100ac06087481dd8e
Craig Prevallet <penguintx@hotmail.com>
2025-11-16 16:37:54 -0600

    Initial commit. Parse a fit file.

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use fitparser::profile::field_types::MesgNum;
+
+fn main() {
+    println!("Parsing FIT files using Profile version: {:?}", fitparser::profile::VERSION);
+    let mut fp = File::open("tests/alsoworking.fit").expect("file not found");
+    while let Ok(data) = fitparser::from_reader(&mut fp) {
+        // print the data in FIT file
+        //println!("{:#?}", data);
+
+        for item in &data {
+            match item.kind() {
+                MesgNum::Record => {
+                    //println!("{:#?}", item.fields());
+                    let x = item.fields().get(0);
+                    println!("{:#?}", x);
+                    let y = item.fields().get(1);
+                    println!("{:#?}", y);
+                    let z = item.fields().get(2);
+                    println!("{:#?}", z);
+                },
+                _ => print!("{}", "") 
+                }
+        }
+    }
+}    
