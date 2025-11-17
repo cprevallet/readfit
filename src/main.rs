f30761aa8f3cbe0a0f85e7f9e90b85131602daba
Craig Prevallet <penguintx@hotmail.com>
2025-11-16 21:49:53 -0600

    Convert semi-circles to degrees.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use std::fs::File;
 use fitparser::profile::field_types::MesgNum;
 use fitparser::FitDataField;
 
+fn semi_to_degrees(semi : i64) -> f64 {
+   let factor : f64 = 2i64.pow(31u32) as f64;
+   let deg_val : f64 = semi as f64 * 180f64 / factor;
+   return deg_val;
+}
+
 fn main() {
     println!("Parsing FIT files using Profile version: {:?}", fitparser::profile::VERSION);
     let mut fp = File::open("tests/alsoworking.fit").expect("file not found");
@@ -11,14 +17,19 @@ fn main() {
         for item in &data {
             match item.kind() {
                 MesgNum::Record => {
-                    //println!("{:#?}", item.fields());
+                     //println!("{:#?}", item.fields());
+                     
                      let pos_lat: &FitDataField = item.fields().get(0).unwrap();
+                     let semi : i64 = pos_lat.value().try_into().expect("conversion failed");  //semicircles
+                     let degrees_lat = semi_to_degrees(semi);
                      println!("Name = {}, Number = {}, Value = {}, Units = {}",
-                         pos_lat.name(), pos_lat.number(), pos_lat.value(), pos_lat.units());
+                         pos_lat.name(), pos_lat.number(), degrees_lat, pos_lat.units());
 
                      let pos_lon: &FitDataField = item.fields().get(1).unwrap();
+                     let semi : i64 = pos_lon.value().try_into().expect("conversion failed");  //semicircles
+                     let degrees_lon = semi_to_degrees(semi);
                      println!("Name = {}, Number = {}, Value = {}, Units = {}",
-                         pos_lon.name(), pos_lon.number(), pos_lon.value(), pos_lon.units());
+                         pos_lon.name(), pos_lon.number(), degrees_lon, pos_lon.units());
                 },
                 _ => print!("{}", "") 
                 }
